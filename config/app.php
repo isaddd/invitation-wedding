5b9637e3d8a1c32271119bbac2ed1ace1b0dca21
Irsyad <irsyadbagas2@gmail.com>
2025-02-03 18:06:32 +0700

    adjust comment

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://irsyad-fanny.vercel.app/'),
 
     'asset_url' => env('ASSET_URL'),
 
